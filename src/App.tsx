c19db1e17ea0f316aaf65336e5441d9b7ecca42c
Eda Dural <edaadurall@gmail.com>
2023-11-23 12:54:57 +0300

    file-folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import './globals.css';
 
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
 const App = () => {
     return (
         <main className='flex h-screen'>
             <Routes>
                 {/* public routes (herkesin kullandığı rotalar) */}
-                <Route path='/sign-in' element={<SiginForm />} />
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
 
                 {/* private routes (özel rotalar) */}
-                <Route index element={<Home />} />
-
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
             </Routes>
         </main>
     )
---
+++ src/_root/pages/index.ts
@@ -0,0 +1,2 @@
+// sahip olunan her bileşenin dışa aktarıLması
+export { default as Home } from './Home';
