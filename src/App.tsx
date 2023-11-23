f8fa06b537159f473e7576f9f623d6776f3815b6
Eda Dural <edaadurall@gmail.com>
2023-11-23 11:37:15 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+const App = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello Snapgram!
+        </h1>
+    )
+}
+
+export default App;
