1713258545f94654ae444c56237046eb5207ece7
Eda Dural <edaadurall@gmail.com>
2023-11-30 17:20:54 +0300

    homepage - left sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 
@@ -23,6 +23,14 @@ const App = () => {
                 {/* private routes (özel rotalar) */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id/*" element={<Profile />} />
+                    <Route path="/update-profile/:id" element={<UpdateProfile />} />
                 </Route>
             </Routes>
 
--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { useSignOutAccount } from '@/lib/react-query/queryAndMutations';
+import { useEffect } from 'react';
+import { useUserContext } from '@/context/AuthContext';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
+import { Button } from '../ui/button';
 
 const LeftSidebar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
   return (
-    <div>LeftSidebar</div>
+    <nav className='leftsidebar'>
+      <div className='flex flex-col gap-11' >
+        <Link
+          to="/"
+          className='flex gap-3 items-center'
+        >
+          <img
+            src='/assets/images/logo.svg'
+            alt='logo'
+            width={160}
+            height={36}
+          />
+        </Link>
+
+        <Link
+          to={`/profile/${user.id}`}
+          className='flex gap-3 items-center'
+        >
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">
+              {user.name}
+            </p>
+            <p className="small-regular text-light-3">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
+
+        <ul className="flex flex-col gap-3">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+
+            return (
+              <li
+                key={link.label}
+                className={`leftsidebar-link group ${isActive && "bg-primary-500"
+                  }`}>
+                <NavLink
+                  to={link.route}
+                  className="flex gap-4 items-center p-4">
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${isActive && "invert-white"
+                      }`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={() => signOut()}>
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
+    </nav>
   )
 }
 
