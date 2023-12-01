4ea7dda11e61554d308295297bca835f15f337b0
Eda Dural <edaadurall@gmail.com>
2023-12-01 11:58:31 +0300

    homepage - bottombar

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,3 +25,26 @@ export const sidebarLinks = [
         label: "Create Post",
     },
 ];
+
+export const bottombarLinks = [
+    {
+        imgURL: "/assets/icons/home.svg",
+        route: "/",
+        label: "Home",
+    },
+    {
+        imgURL: "/assets/icons/wallpaper.svg",
+        route: "/explore",
+        label: "Explore",
+    },
+    {
+        imgURL: "/assets/icons/bookmark.svg",
+        route: "/saved",
+        label: "Saved",
+    },
+    {
+        imgURL: "/assets/icons/gallery-add.svg",
+        route: "/create-post",
+        label: "Create",
+    },
+];
