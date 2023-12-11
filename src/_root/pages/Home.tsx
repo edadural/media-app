6c1c0a001d3873d829a00a035283feda42781603
Eda Dural <edaadurall@gmail.com>
2023-12-11 09:36:26 +0300

    home

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Loader from "@/components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
-import { useGetRecentPosts } from "@/lib/react-query/queryAndMutations";
+import UserCard from "@/components/shared/UserCard";
+import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queryAndMutations";
 import { Models } from "appwrite";
 
 const Home = () => {
@@ -11,11 +12,30 @@ const Home = () => {
         isError: isErrorPosts,
     } = useGetRecentPosts();
 
+    const {
+        data: creators,
+        isLoading: isUserLoading,
+        isError: isErrorCreators,
+    } = useGetUsers(10);
+
+    if (isErrorPosts || isErrorCreators) {
+        return (
+            <div className="flex flex-1">
+                <div className="home-container">
+                    <p className="body-medium text-light-1">Something bad happened</p>
+                </div>
+                <div className="home-creators">
+                    <p className="body-medium text-light-1">Something bad happened</p>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="flex flex-1">
             <div className="home-container">
                 <div className="home-posts">
-                    <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+                    <h2 className="h3-bold md:h2-bold text-left w-full">Anasayfa</h2>
                     {isPostLoading && !posts ? (
                         <Loader />
                     ) : (
@@ -26,6 +46,21 @@ const Home = () => {
                         </ul>
                     )}
                 </div>
+            </div>
+
+            <div className="home-creators">
+                <h3 className="h3-bold text-light-1">Kullanıcılar</h3>
+                {isUserLoading && !creators ? (
+                    <Loader />
+                ) : (
+                    <ul className="grid 2xl:grid-cols-2 gap-6">
+                        {creators?.documents.map((creator) => (
+                            <li key={creator?.$id}>
+                                <UserCard user={creator} />
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     )
