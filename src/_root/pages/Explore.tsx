9b285862b48799ad1b2cec24c1cdea0416d03563
Eda Dural <edaadurall@gmail.com>
2023-12-11 13:10:51 +0300

    liked posts

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -34,7 +34,7 @@ const Explore = () => {
   return (
     <div className="explore-container" >
       <div className="explore-inner_container">
-        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        <h2 className="h3-bold md:h2-bold w-full">Gönderi Ara</h2>
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
           <img
             src="/assets/icons/search.svg"
@@ -44,7 +44,7 @@ const Explore = () => {
           />
           <Input
             type="text"
-            placeholder="Search"
+            placeholder="Arama"
             className="explore-search"
             value={searchValue}
             onChange={(e) => {
@@ -56,10 +56,10 @@ const Explore = () => {
       </div>
 
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+        <h3 className="body-bold md:h3-bold">Gündemdekiler</h3>
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          <p className="small-medium md:base-medium text-light-2">All</p>
+          <p className="small-medium md:base-medium text-light-2">Hepsi</p>
           <img
             src="/assets/icons/filter.svg"
             width={20}
@@ -76,7 +76,7 @@ const Explore = () => {
             searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
-          <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
+          <p className="text-light-4 mt-10 text-center w-full">Gönderinin son</p>
         ) :
           posts.pages.map((item, index) => (
             <GridPostList key={`page-${index}`} posts={item.documents} />
--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queryAndMutations";
 
 const LikedPosts = () => {
-  return (
-    <div>LikedPosts</div>
-  )
-}
+  const { data: currentUser } = useGetCurrentUser();
 
-export default LikedPosts
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">Beğenilen gönderi yok</p>
+      )}
+
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+};
+
+export default LikedPosts;
